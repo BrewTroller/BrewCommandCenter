ffc7621f60d28555734e66bbe9a0dadab4313ba6
mrshake <adam@adamandnicole.com>
2016-08-12 13:17:10 -0500

    Intial Design Idea and wireframe

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Brew Command Center</title>
+    <link rel="stylesheet" type="text/css" href="main.css">
+  </head>
+  <body>
+    <div class="main_Container">
+      <div class="header">
+        <p>
+          <h1>Brew Command Center</h1>
+        </p>
+      </div>
+      <div class="nav">
+        Home | Recipes | Forum | Breweries
+      </div>
+      <div class="content_Container">
+        <div class="card leftCard">
+          <div class="cardHeader">
+            <h3>Plan</h3>
+          </div>
+          Test
+        </div>
+        <div class="card middleCard">
+          <div class="cardHeader">
+            <h3>Aquire</h3>
+          </div>
+          Test
+        </div>
+        <div class="card rightCard">
+          <div class="cardHeader">
+            <h3>Brew</h3>
+          </div>
+          Test
+        </div>
+        <div class="longAccordian">
+          Tools
+        </div>
+      </div>
+      </nav>
+    </div>
+  </body>
+</html>
